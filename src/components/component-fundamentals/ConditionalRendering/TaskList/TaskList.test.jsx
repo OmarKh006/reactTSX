017171ace908e542aaa99f1fec2bf1117dbf486b
OmarKh006 <khairyomar006@gmail.com>
2026-07-17 02:50:40 +0300

    to be defined

--- src/components/component-fundamentals/ConditionalRendering/TaskList/TaskList.test.jsx
+++ src/components/component-fundamentals/ConditionalRendering/TaskList/TaskList.test.jsx
@@ -1,9 +1,12 @@
-import { describe, test } from "vitest";
-import { render } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
 import TaskList from ".";
 
 describe("TaskList component functions", () => {
   test("renders TaskList component", () => {
     render(<TaskList />);
+    expect(screen.getByText("Learn React")).toBeDefined();
+    expect(screen.getByText("Practice Props")).toBeDefined();
+    expect(screen.getByText("Take a Break")).toBeDefined();
   });
 });
